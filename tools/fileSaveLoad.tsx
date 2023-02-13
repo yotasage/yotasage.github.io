6d66cc958765dc5ca04a548d2e399148b95138a0
Jonathan Sæthre Ege <1599278+TheJonaMr@users.noreply.github.com>
2023-02-13 23:03:59 +0100

    Fixed: Converted javascript code to TypeScript.

--- tools/fileSaveLoad.tsx
+++ tools/fileSaveLoad.tsx
@@ -11,8 +11,8 @@ export function saveMap(mapData: IMap) {
 
 // https://stackoverflow.com/questions/3665115/how-to-create-a-file-in-memory-for-user-to-download-but-not-through-server
 // https://stackoverflow.com/a/18197341/2883691
-export function download(filename, text) {
-    var element = document.createElement('a');
+export function download(filename: string, text: string) {
+    let element: HTMLAnchorElement = document.createElement('a');
     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
     element.setAttribute('download', filename);
 
