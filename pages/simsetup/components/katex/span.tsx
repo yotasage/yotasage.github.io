cddefb58694a6e88a7c85ffd2d8e2ca9517b34bf
Jonathan Sæthre Ege <jonathan.s.ege@ntnu.no>
2024-12-01 00:29:04 +0100

    TEMP_FIX: Probems with KaTeX, but at least building the website is working now. Having trouble with double output from KaTeX.

--- pages/simsetup/components/katex/span.tsx
+++ pages/simsetup/components/katex/span.tsx
@@ -2,26 +2,22 @@
 
 // https://github.com/kirankulkarni/next-katex
 
-import renderMathInElement from './../../../../node_modules/katex/dist/contrib/auto-render';
-import 'katex/dist/katex.min.css';
+import katex from 'katex';
+// import renderMathInElement from './../../../../node_modules/katex/dist/contrib/auto-render';
+// import 'katex/dist/katex.min.css';
 import { useEffect, useRef } from 'react';
 
 export default function KatexSpan({ text, ...delegated }) {
   const katexTextRef = useRef();
   useEffect(() => {
     if (katexTextRef.current) {
-      renderMathInElement(katexTextRef.current, {
-        delimiters: [
-          { left: '$$', right: '$$', display: true },
-          { left: '$', right: '$', display: false },
-        ],
-      });
+      katex.render(text, katexTextRef.current as HTMLElement);
     }
   }, [text]);
 
   return (
     <div ref={katexTextRef} {...delegated}>
-      {text}
+      
     </div>
   );
 }
